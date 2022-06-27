b9453fdd8a292ed53258a784f80039d20c7e3311
ona-hyejinkim <hyejin.kim@ona.co.kr>
2022-06-27 16:19:48 +0900

    pc > 캘린더작업, m 캘린더 수정

--- src/assets/js/datepicker.js
+++ src/assets/js/datepicker.js
@@ -1,6 +1,6 @@
 $( function() {
   $.datepicker.setDefaults({
-    dateFormat: 'yy-mm-dd',
+    dateFormat: 'yy.mm.dd',
     prevText: '',
     nextText: '',
     monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
@@ -9,7 +9,7 @@ $( function() {
     dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
     dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
     showMonthAfterYear: true,
-
+    yearSuffix: '년',
 });
 
 
